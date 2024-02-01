a478d5de1684730b447b91f9c7b4a2e89cb9643f
Maulana Y <maulanayusuuff@gmail.com>
2024-02-01 13:59:49 +0700

    PAWTB-2 <message>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Picuppy" }');
+    res.send('{ "response": "Hello From hi" }');
 });
 
 app.get('/will', function (req, res) {
